47c572260b88be1f0f0e7edbf79f7e37edf21d26
Josef Kahoun <48121432+kahy9@users.noreply.github.com>
2021-05-09 19:20:48 +0200

    Updated generation logic + directions;  Fixed borders

--- main.js
+++ main.js
@@ -5,107 +5,177 @@ const tileSize = 50;
 const tileCountX = canvas.width / tileSize;
 const tileCountY = canvas.height / tileSize;
 
-let PosY = tileSize * 1;
-let PosX = tileSize * 1;
+const arr = Array.from(Array(tileCountX), () => new Array(tileCountY));
+const FLOOR_COLOR = "#FBE000";
+const BORDER_COLOR = "#FF0080";
+const INNER_BORDER_COLOR = "#FF00FF";
 
-let borderY = 0;
-let borderX = 0;
-let getTile = 1;
-
-let direction = 0;
-
-let arr = [];
-let arrBasis = [];
+const directions = ["UP", "DOWN", "LEFT", "RIGHT"];
+let direction;
+let basis;
+let base;
 
 const drawGrid = () => {
-    for (let i = 0; i < tileCountX; i++) {
-        for (let j = 0; j < tileCountY; j++) {
-            if (i === 0 || i === tileCountX - 1 || j === 0 || j === tileCountY - 1) {
-                rectangle(
-                    "#FF0080",
-                    tileSize * i,
-                    tileSize * j,
-                    tileSize - 1,
-                    tileSize - 1,
-                );
-                arr.push({
-                    tile: {
-                        X: i,
-                        Y: j,
-                        value: 0
-                    }
-                });
-            } else if (i % 2 === 0 && j % 2 === 0) {
-                rectangle(
-                    "#FF0080",
-                    tileSize * i,
-                    tileSize * j,
-                    tileSize - 1,
-                    tileSize - 1,
-                );
-                arrBasis.push({
-                    tile: {
-                        X: i,
-                        Y: j,
-                        value: 1
-                    }
-                });
-            } else {
-                rectangle(
-                    "#FBE000",
-                    tileSize * i,
-                    tileSize * j,
-                    tileSize - 1,
-                    tileSize - 1
-                );
-                arr.push({
-                    tile: {
-                        X: i,
-                        Y: j,
-                        value: 2
-                    }
-                });
-            }
-        }
+  for (let i = 0; i < tileCountX; i++) {
+    for (let j = 0; j < tileCountY; j++) {
+      if (i === 0 || i === tileCountX - 1 || j === 0 || j === tileCountY - 1) {
+        rectangle(
+          BORDER_COLOR,
+          tileSize * i,
+          tileSize * j,
+          tileSize - 1,
+          tileSize - 1
+        );
+        arr[i][j] = 0;
+      } else if (i % 2 === 0 && j % 2 === 0) {
+        rectangle(
+          BORDER_COLOR,
+          tileSize * i,
+          tileSize * j,
+          tileSize - 1,
+          tileSize - 1
+        );
+        arr[i][j] = 1;
+      } else {
+        rectangle(
+          FLOOR_COLOR,
+          tileSize * i,
+          tileSize * j,
+          tileSize - 1,
+          tileSize - 1
+        );
+        arr[i][j] = 2;
+      }
     }
-}
+  }
+};
 
-const drawInnerBorder = () => {
-    const getRandomBasis = Math.floor(Math.random() * arrBasis.length);
-    getDirection();
-    goTop(getRandomBasis);
-    console.log(arrBasis);
-}
+const drawInnerBorders = () => {
+  let basisCount = getBasisCount();
+  let counter = 0;
+  let randomBase = getRandomBase(basisCount);
+  for (let i = 0; i < tileCountX; i++) {
+    for (let j = 0; j < tileCountY; j++) {
+      if (arr[i][j] === 1) {
+        counter += 1;
+        if (counter === randomBase) {
+          direction = getRandomDirection();
+          switch (direction) {
+            case "UP":
+              goTop(i, j);
+              break;
+            case "DOWN":
+              goDown(i, j);
+              break;
+            case "LEFT":
+              goLeft(i, j);
+              break;
+            case "RIGHT":
+              goRight(i, j);
+              break;
+            default:
+              break;
+          }
+        }
+      }
+    }
+  }
+};
 
-const getDirection =  () => {
-    return Math.floor(Math.random() * 4);
-}
+const getBasisCount = () => {
+  basis = 0;
+  for (let i = 0; i < tileCountX; i++) {
+    for (let j = 0; j < tileCountY; j++) {
+      if (arr[i][j] === 1) {
+        basis += 1;
+      }
+    }
+  }
+  return basis;
+};
+
+const getRandomBase = (basisCount) => {
+  base = Math.floor(Math.random() * basisCount) + 1;
+  return base;
+};
+
+const getRandomDirection = () => {
+  let random = Math.floor(Math.random() * 4);
+  return directions[random];
+};
+
+const goTop = (i, j) => {
+  if (arr[i][j] !== 0) {
+    rectangle(
+      INNER_BORDER_COLOR,
+      tileSize * i,
+      tileSize * j,
+      tileSize - 1,
+      tileSize - 1
+    );
+    arr[i][j] = 0;
+    goTop(i, j - 1);
+  } else {
+    drawInnerBorders();
+  }
+};
+
+const goDown = (i, j) => {
+  if (arr[i][j] !== 0) {
+    rectangle(
+      INNER_BORDER_COLOR,
+      tileSize * i,
+      tileSize * j,
+      tileSize - 1,
+      tileSize - 1
+    );
+    arr[i][j] = 0;
+    goDown(i, j + 1);
+  } else {
+    drawInnerBorders();
+  }
+};
+
+const goLeft = (i, j) => {
+  if (arr[i][j] !== 0) {
+    rectangle(
+      INNER_BORDER_COLOR,
+      tileSize * i,
+      tileSize * j,
+      tileSize - 1,
+      tileSize - 1
+    );
+    arr[i][j] = 0;
+    goLeft(i - 1, j);
+  } else {
+    drawInnerBorders();
+  }
+};
+
+const goRight = (i, j) => {
+  if (arr[i][j] !== 0) {
+    rectangle(
+      INNER_BORDER_COLOR,
+      tileSize * i,
+      tileSize * j,
+      tileSize - 1,
+      tileSize - 1
+    );
+    arr[i][j] = 0;
+    goRight(i + 1, j);
+  } else {
+    drawInnerBorders();
+  }
+};
 
 const rectangle = (color, x, y, width, height) => {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, width, height, tileSize - 1, tileSize - 1);
-}
-
-const goTop = (getRandomBasis) => {
-    if (arrBasis[getRandomBasis].tile.Y - 1 !== 0) {
-        rectangle(
-            "#FF0000",
-            (arrBasis[getRandomBasis].tile.X) * tileSize,
-            (arrBasis[getRandomBasis].tile.Y - topDir) * tileSize,
-            tileSize - 1,
-            tileSize - 1,
-        );
-        topDir += 1;
-        goTop(getRandomBasis);
-    } else {
-        topDir = 0;
-        arrBasis.splice(getRandomBasis);
-    }
-}
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height, tileSize - 1, tileSize - 1);
+};
 
 const gameDraw = () => {
-    drawGrid();
-    drawInnerBorder();
-}
+  drawGrid();
+  drawInnerBorders();
+};
 
 gameDraw();
